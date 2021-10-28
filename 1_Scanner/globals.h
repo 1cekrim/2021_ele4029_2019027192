6393246726a6a8084696fbc074b3ab6497ef54e5
1cekrim <1cekrim.dev@gmail.com>
2021-10-29 00:43:34 +0900

    [style] Format code

--- 1_Scanner/globals.h
+++ 1_Scanner/globals.h
@@ -15,30 +15,56 @@
 #include <string.h>
 
 #ifndef FALSE
-#define FALSE 0
+    #define FALSE 0
 #endif
 
 #ifndef TRUE
-#define TRUE 1
+    #define TRUE 1
 #endif
 
 /* MAXRESERVED = the number of reserved words */
 #define MAXRESERVED 6
 
-typedef enum 
-    /* book-keeping tokens */
-   {ENDFILE,ERROR,
+typedef enum
+/* book-keeping tokens */
+{
+    ENDFILE,
+    ERROR,
     /* reserved words */
-    IF,ELSE,WHILE,RETURN,INT,VOID,
+    IF,
+    ELSE,
+    WHILE,
+    RETURN,
+    INT,
+    VOID,
     /* multicharacter tokens */
-    ID,NUM,
+    ID,
+    NUM,
     /* special symbols */
-    ASSIGN,EQ,NE,LT,LE,GT,GE,PLUS,MINUS,TIMES,OVER,LPAREN,RPAREN,LBRACE,RBRACE,LCURLY,RCURLY,SEMI,COMMA
-   } TokenType;
+    ASSIGN,
+    EQ,
+    NE,
+    LT,
+    LE,
+    GT,
+    GE,
+    PLUS,
+    MINUS,
+    TIMES,
+    OVER,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    LCURLY,
+    RCURLY,
+    SEMI,
+    COMMA
+} TokenType;
 
-extern FILE* source; /* source code text file */
+extern FILE* source;  /* source code text file */
 extern FILE* listing; /* listing output text file */
-extern FILE* code; /* code text file for TM simulator */
+extern FILE* code;    /* code text file for TM simulator */
 
 extern int lineno; /* source line number for listing */
 
@@ -46,26 +72,55 @@ extern int lineno; /* source line number for listing */
 /***********   Syntax tree for parsing ************/
 /**************************************************/
 
-typedef enum {StmtK,ExpK} NodeKind;
-typedef enum {IfK,RepeatK,AssignK,ReadK,WriteK} StmtKind;
-typedef enum {OpK,ConstK,IdK} ExpKind;
+typedef enum
+{
+    StmtK,
+    ExpK
+} NodeKind;
+typedef enum
+{
+    IfK,
+    RepeatK,
+    AssignK,
+    ReadK,
+    WriteK
+} StmtKind;
+typedef enum
+{
+    OpK,
+    ConstK,
+    IdK
+} ExpKind;
 
 /* ExpType is used for type checking */
-typedef enum {Void,Integer,Boolean} ExpType;
+typedef enum
+{
+    Void,
+    Integer,
+    Boolean
+} ExpType;
 
 #define MAXCHILDREN 3
 
 typedef struct treeNode
-   { struct treeNode * child[MAXCHILDREN];
-     struct treeNode * sibling;
-     int lineno;
-     NodeKind nodekind;
-     union { StmtKind stmt; ExpKind exp;} kind;
-     union { TokenType op;
-             int val;
-             char * name; } attr;
-     ExpType type; /* for type checking of exps */
-   } TreeNode;
+{
+    struct treeNode* child[MAXCHILDREN];
+    struct treeNode* sibling;
+    int lineno;
+    NodeKind nodekind;
+    union
+    {
+        StmtKind stmt;
+        ExpKind exp;
+    } kind;
+    union
+    {
+        TokenType op;
+        int val;
+        char* name;
+    } attr;
+    ExpType type; /* for type checking of exps */
+} TreeNode;
 
 /**************************************************/
 /***********   Flags for tracing       ************/
@@ -100,5 +155,5 @@ extern int TraceAnalyze;
 extern int TraceCode;
 
 /* Error = TRUE prevents further passes if an error occurs */
-extern int Error; 
+extern int Error;
 #endif
